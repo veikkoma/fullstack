5c4533f28555ddae12a8b73413f00cf531afefc7
veikkoma <matias.veikkola@gmail.com>
2024-11-21 00:03:12 +0200

    part 1.4 done - parts converted to table

--- part1/kurssitiedot/src/App.jsx
+++ part1/kurssitiedot/src/App.jsx
@@ -35,21 +35,21 @@ const Total = ({ parts }) => {
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  };
-
-  /* Helps use - parts better inb content! */
-  const parts = [part1, part2, part3];
+  /* Now I transformed the parts into a table - variable definitions! */
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
