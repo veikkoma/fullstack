39f073f52284b8fc559fd15e53185f0f85313e30
veikkoma <matias.veikkola@gmail.com>
2024-11-20 23:16:37 +0200

    add part 1.1 files

---
+++ part1/kurssitiedot/src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css'
+import React from 'react'
+
+const Header = ({ course }) => {
+  return <h1>{course}</h1>
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <p key={index}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+    </div>
+  )
+}
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return <p>Number of exercises {total}</p>
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default App
