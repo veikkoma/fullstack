e30508a81a98a5e22181d72aef425d610c57bf5f
veikkoma <matias.veikkola@gmail.com>
2024-11-20 23:55:12 +0200

    Done 1.3 - add some consolelogs! Helps understant what's happening

--- part1/kurssitiedot/src/App.jsx
+++ part1/kurssitiedot/src/App.jsx
@@ -1,40 +1,55 @@
 import './App.css'
-import React from 'react'
+import React from 'react';
 
 const Header = ({ course }) => {
-  return <h1>{course}</h1>
-}
+  console.log('Header props:', { course });
+  return <h1>{course}</h1>;
+};
 
 const Part = ({ name, exercises }) => {
+  console.log('Part props:', { name, exercises });
   return (
     <p>
       {name} {exercises}
     </p>
-  )
-}
+  );
+};
 
 const Content = ({ parts }) => {
+  console.log('Content props:', { parts });
   return (
     <div>
       <Part name={parts[0].name} exercises={parts[0].exercises} />
       <Part name={parts[1].name} exercises={parts[1].exercises} />
       <Part name={parts[2].name} exercises={parts[2].exercises} />
     </div>
-  )
-}
+  );
+};
 
 const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-  return <p>Number of exercises {total}</p>
-}
+  console.log('Total props:', { parts });
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises;
+  console.log('Total exercises:', total);
+  return <p>Number of exercises {total}</p>;
+};
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 }
-  ]
+  const course = 'Half Stack application development';
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  };
+
+  /* Helps use - parts better inb content! */
+  const parts = [part1, part2, part3];
 
   return (
     <div>
@@ -42,7 +57,7 @@ const App = () => {
       <Content parts={parts} />
       <Total parts={parts} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
