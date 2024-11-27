f9f712e5fc3cf46363149846c608954e263692ce
veikkoma <matias.veikkola@gmail.com>
2024-11-27 22:49:56 +0200

    part 1.12 - add anecdotes. Keep unicafe there asswell

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -3,9 +3,34 @@ import Statistics from './Statistics'; // new file to statistics - make code cle
 import Button from './Button'; // Button file! 
 
 const App = () => {
+
+  // Unicage: const to feedback
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+
+  // anecdotes: const to anecdotes
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.',
+  ];
+
+  const [selected, setSelected] = useState(0);
+
+  // Random select anecdote! 
+  const selectRandomAnecdote = () => {
+    let randomIndex;
+    do {
+      randomIndex = Math.floor(Math.random() * anecdotes.length);
+    } while (randomIndex === selected);
+    setSelected(randomIndex);
+  };
 
   return (
     <div>
@@ -18,6 +43,13 @@ const App = () => {
 
       {/* Statistics components! */}
       <Statistics good={good} neutral={neutral} bad={bad} />
+
+      {/* Anecdotes data! */}
+      <hr />
+      <h1>Anecdotes</h1>
+      <h2>Anecdote of the day</h2>
+      <p>{anecdotes[selected]}</p>
+      <Button text="Next anecdote" handleClick={selectRandomAnecdote} />
     </div>
   );
 };
