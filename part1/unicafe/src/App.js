02c7de5f5959af4c719001b2b31f258519bc233a
veikkoma <matias.veikkola@gmail.com>
2024-11-27 22:20:29 +0200

    1.7 - more statistics!

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,6 +5,12 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  // Laskelmat
+  const total = good + neutral + bad; 
+  const average = total === 0 ? 0 : (good - bad) / total; 
+  const positivePercentage = total === 0 ? 0 : (good / total) * 100; 
+
+
   return (
     <div>
       <h1>UNICAFE - Give feedback</h1>
@@ -12,13 +18,22 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
       <button onClick={() => setBad(bad + 1)}>Bad</button>
 
-      <h2>Feedback Statistics</h2>
-      {/* feedback statistics */} 
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-    </div>
-  );
-};
+    {/* feedback statistics  - add total average and positive % ? */}
+    <h2>feedback statistics</h2>
+        {total === 0 ? (
+          <p>No feedback</p>
+        ) : (
+          <div>
+            <p>Good: {good}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Bad: {bad}</p>
+            <p>Total: {total}</p>
+            <p>Average: {average.toFixed(1)}</p>
+            <p>Positive (%): {positivePercentage.toFixed(1)}%</p>
+          </div>
+        )}
+      </div>
+    );
+  };
 
 export default App;
