2d615f1a79cc0e49a32d54af7a9f8f27e2acf17f
veikkoma <matias.veikkola@gmail.com>
2024-11-27 22:26:30 +0200

    part 1.8 - nnew statistics component!

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,39 +1,24 @@
 import { useState } from 'react';
+import Statistics from './Statistics'; // new file to statistics - make code clean and more readable!
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  // Laskelmat
-  const total = good + neutral + bad; 
-  const average = total === 0 ? 0 : (good - bad) / total; 
-  const positivePercentage = total === 0 ? 0 : (good / total) * 100; 
-
-
   return (
     <div>
-      <h1>UNICAFE - Give feedback</h1>
+      <h1>Unicafe - give feedback</h1>
+
+      {/* feedback clicks*/}
       <button onClick={() => setGood(good + 1)}>Good</button>
       <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
       <button onClick={() => setBad(bad + 1)}>Bad</button>
 
-    {/* feedback statistics  - add total average and positive % ? */}
-    <h2>feedback statistics</h2>
-        {total === 0 ? (
-          <p>No feedback</p>
-        ) : (
-          <div>
-            <p>Good: {good}</p>
-            <p>Neutral: {neutral}</p>
-            <p>Bad: {bad}</p>
-            <p>Total: {total}</p>
-            <p>Average: {average.toFixed(1)}</p>
-            <p>Positive (%): {positivePercentage.toFixed(1)}%</p>
-          </div>
-        )}
-      </div>
-    );
-  };
+      {/* Statistics components! */}
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
 
 export default App;
---
+++ part1/unicafe/src/Statistics.js
@@ -0,0 +1,46 @@
+const Statistics = ({ good, neutral, bad }) => {
+    const total = good + neutral + bad;
+    const average = total === 0 ? 0 : (good - bad) / total;
+    const positivePercentage = total === 0 ? 0 : (good / total) * 100;
+  
+    return (
+      <div>
+        <h2>Statistics</h2>
+        {total === 0 ? (
+          <p>no feedback</p>
+        ) : (
+          <table>
+            <tbody>
+              <tr>
+                <td>Good</td>
+                <td>{good}</td>
+              </tr>
+              <tr>
+                <td>Neutral</td>
+                <td>{neutral}</td>
+              </tr>
+              <tr>
+                <td>Bad</td>
+                <td>{bad}</td>
+              </tr>
+              <tr>
+                <td>Total</td>
+                <td>{total}</td>
+              </tr>
+              <tr>
+                <td>Average</td>
+                <td>{average.toFixed(1)}</td>
+              </tr>
+              <tr>
+                <td>Positive (%)</td>
+                <td>{positivePercentage.toFixed(1)}%</td>
+              </tr>
+            </tbody>
+          </table>
+        )}
+      </div>
+    );
+  };
+  
+  export default Statistics;
+  
