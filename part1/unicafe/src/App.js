9a1244ebd970ea4bf80acc6d8398088c44a53091
veikkoma <matias.veikkola@gmail.com>
2024-11-27 22:16:13 +0200

    1.6 part done - new react app

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>UNICAFE - Give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+
+      <h2>Feedback Statistics</h2>
+      {/* feedback statistics */} 
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  );
+};
+
+export default App;
