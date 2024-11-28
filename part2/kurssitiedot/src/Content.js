c7b8c3309b3faaa454e5b4a575f60ac9632b6c09
veikkoma <matias.veikkola@gmail.com>
2024-11-29 00:14:32 +0200

    part 2.4 - extended as in the task

--- part2/kurssitiedot/src/Content.js
+++ part2/kurssitiedot/src/Content.js
@@ -3,15 +3,14 @@ import Part from './Part';
 
 const Content = ({ parts }) => {
   console.log('CONTENT component parts:', parts);
-
+  
   return (
     <div>
-      {parts.map((part) => {
-        console.log('rendering part : ', part);
-        return <Part key={part.id} name={part.name} exercises={part.exercises} />;
-      })}
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
     </div>
-  );
+  );  
 };
 
 export default Content;
