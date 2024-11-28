c261830b182aa27f93f136220bc0fa23e1a68037
veikkoma <matias.veikkola@gmail.com>
2024-11-29 01:01:07 +0200

    part 2.9 - add filter, hardcode users from example

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -4,12 +4,15 @@ const App = () => {
   // state keep track of the list (persons)
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: "040-30239849" },
-    { name: 'Grace Hopper', number: "044-21233435" }
+    { name: 'Grace Hopper', number: "044-21233435" },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]);
 
   // states to name and number
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
 
   // add new person!
@@ -45,9 +48,23 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  // handle filter changes
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const filterPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+      Filter shown with:  
+      <input value={filter} onChange={handleFilterChange} />
+      </div>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -59,13 +76,13 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h3>Numbers</h3>
       <ul>
-        {persons.map((person) => (
-          <li key={`${person.name}-${person.number}`}>
-          {person.name} {person.number}
-            </li>
-          ))}
+        {filterPersons.map((person) => (
+          <li key={person.name}>
+            {person.name} {person.number}
+          </li>
+        ))}
       </ul>
     </div>
   );
